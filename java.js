30cfac69e22d897e51f38381e5e1afb89d464f54
RashidmMunir001 <rashid_ntu1081@outlook.com>
2021-02-17 13:40:16 +0500

    another commit of html,css,js

--- java.js
+++ java.js
@@ -1,3 +1,24 @@
-function.myFunction{
-  document.getElementById('java').innerHTML="rashids javascript";
+
+var pi= 3.14;
+var little= "awais";
+var small= "hassan";
+document.getEelementById("string").innerHTML=pi + "<br>" + little + "<br>" + small;
+</script>
+<script>
+var carname="toyota ,suzuki,honda";
+
+function myFunc(){
+document.getElementById("car").innerHTML="cars name are:" +  carname;
 }
+myFunc();
+</script>
+
+
+//javascript has two types of values
+//literals and variables
+//literals are numbers(with/wihtout) decimals, strings are words
+//variables are x,y,z or any name other than reserved words, use to store values
+//e.g var x,y;
+//x=2,y=4;
+//js uses arithmetic operators to store values
+//like +,-,*,/
